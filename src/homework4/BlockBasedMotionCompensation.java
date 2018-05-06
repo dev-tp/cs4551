4eb4be008a2dc596010121c24691ab545f9ff4fb
Eryk <rcksorto@yahoo.com>
2018-05-05 20:58:14 -0700

    Exhaustive Block Motion Compensation based on MATLAB code
    https://www.mathworks.com/matlabcentral/fileexchange/8761-block-matching-algorithms-for-motion-estimation

--- src/homework4/BlockBasedMotionCompensation.java
+++ src/homework4/BlockBasedMotionCompensation.java
@@ -4,56 +4,105 @@ import utils.Image;
 
 class BlockBasedMotionCompensation {
 
+    private static int[][] macroBlock(Image image, int x0, int x1, int y0, int y1) {
+        int m = x1 - x0;
+        int n = y1 - y0;
+
+        int[][] macroBlock = new int[m][n];
+
+        int[] rgb = new int[3];
+
+        for (int i = x0; i < x1; i++) {
+            for (int j = y0; j < y1; j++) {
+                image.getPixel(i, j, rgb);
+
+                int grayValue = (int) (0.299 * rgb[0] + 0.587 * rgb[1] + 0.114 * rgb[2]);
+
+                if (grayValue < 0) {
+                    grayValue = 0;
+                } else if (grayValue > 255) {
+                    grayValue = 255;
+                }
+
+                macroBlock[i % m][j % n] = grayValue;
+            }
+        }
+
+        return macroBlock;
+    }
+
+    private static double meanSquareDifference(int[][] targetBlock, int[][] referenceBlock, int blockSize) {
+        double sum = 0.0;
+
+        // ΣΣ(A[p, q] - B[p, q])^2
+        for (int p = 0; p < blockSize; p++) {
+            for (int q = 0; q < blockSize; q++) {
+                sum += Math.pow(targetBlock[p][q] - referenceBlock[p][q], 2.0);
+            }
+        }
+
+        // 1/mn ΣΣ(A[p, q] - B[p, q])^2
+        return sum / (blockSize * blockSize);
+    }
+
+    private static Number[] motionVectorAndMinCost(double[][] costs) {
+        Number[] tuple = {0, 0, costs[0][0]}; // dx, dy, minCost
+
+        for (int x = 0; x < costs.length; x++) {
+            for (int y = 0; y < costs[x].length; y++) {
+                if (costs[x][y] < tuple[2].doubleValue()) {
+                    tuple[0] = x;
+                    tuple[1] = y;
+                    tuple[2] = costs[x][y];
+                }
+            }
+        }
+
+        return tuple;
+    }
+
+    private static void resetCostValues(double[][] costs) {
+        for (int x = 0; x < costs.length; x++) {
+            for (int y = 0; y < costs[x].length; y++) {
+                costs[x][y] = 0.0;
+            }
+        }
+    }
+
     static void routine(Image targetImage, Image referenceImage, int blockSize, int searchWindow) {
-        int block = 0;
         int height = targetImage.getHeight();
         int width = targetImage.getWidth();
 
-        int[] grayValues = new int[2];
-        int[] rgb = new int[3];
-        double[] sumPerBlock = new double[(width / blockSize) * (height / blockSize)];
+        double[][] costs = new double[2 * searchWindow + 1][2 * searchWindow + 1];
 
         // Divide target image into nxn blocks
         for (int y = 0; y < height; y += blockSize) {
             for (int x = 0; x < width; x += blockSize) {
 
-                // Apply Mean Square Difference (MSD) on each block
-                for (int p = 0; p < blockSize; p++) {
-                    for (int q = 0; q < blockSize; q++) {
-                        targetImage.getPixel(x + p, y + q, rgb);
-                        grayValues[0] = (int) (0.299 * rgb[0] + 0.587 * rgb[1] + 0.114 * rgb[2]);
+                int[][] a = macroBlock(targetImage, x, x + blockSize, y, y + blockSize);
 
-                        referenceImage.getPixel(x + p, y + q, rgb);
-                        grayValues[1] = (int) (0.299 * rgb[0] + 0.587 * rgb[1] + 0.114 * rgb[2]);
+                for (int m = -searchWindow; m <= searchWindow; m++) {
+                    for (int n = -searchWindow; n <= searchWindow; n++) {
+                        int refX = x + m;
+                        int refY = y + n;
 
-                        for (int i = 0; i < grayValues.length; i++) {
-                            if (grayValues[i] < 0) {
-                                grayValues[i] = 0;
-                            } else if (grayValues[i] > 255) {
-                                grayValues[i] = 255;
-                            }
+                        if (refX < 0 || refX + blockSize > width || refY < 0 || refY + blockSize > height) {
+                            continue;
                         }
 
-                        // ΣΣ(A[p, q] - B[p, q])^2
-                        sumPerBlock[block] += Math.pow(grayValues[0] - grayValues[1], 2.0);
+                        int[][] b = macroBlock(referenceImage, refX, refX + blockSize, refY, refY + blockSize);
+
+                        costs[m + searchWindow][n + searchWindow] = meanSquareDifference(a, b, blockSize);
                     }
                 }
 
-                // 1/mn ΣΣ(A[p, q] - B[p, q])^2
-                sumPerBlock[block] = (1.0 / (blockSize * blockSize) * sumPerBlock[block++]);
+                Number[] min = motionVectorAndMinCost(costs);
+
+                System.out.println((min[0].intValue() - searchWindow) + " " + (min[1].intValue() - searchWindow) + " "
+                        + min[2]);
+
+                resetCostValues(costs);
             }
         }
-
-        double min = sumPerBlock[0];
-        int index = 0;
-
-        for (int i = 0; i < sumPerBlock.length; i++) {
-            if (sumPerBlock[i] < min) {
-                min = sumPerBlock[i];
-                index = i;
-            }
-        }
-
-        System.out.println(index + ": " + min);
     }
 }
